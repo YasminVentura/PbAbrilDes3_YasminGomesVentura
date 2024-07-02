7d8c90107c7c49c55b67f93c9b4d69163ba84da8
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-07-02 10:07:28 -0300

    swagger pt 2

--- projeto-ms/ms-calculate/src/main/java/org/yasmingv/ms_calculate/aplicacao/CalculadorControlador.java
+++ projeto-ms/ms-calculate/src/main/java/org/yasmingv/ms_calculate/aplicacao/CalculadorControlador.java
@@ -1,11 +1,17 @@
 package org.yasmingv.ms_calculate.aplicacao;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.yasmingv.ms_calculate.aplicacao.dto.CalculadorDTO;
 import org.yasmingv.ms_calculate.aplicacao.dto.CalculadorResposta;
 
+@Tag(name = "Calculador", description = "Endpoints relacionado ao calculo dos pontos")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/v1/calculate")
@@ -14,6 +20,10 @@ public class CalculadorControlador {
     private final CalculadorServico servico;
 
     @PostMapping
+    @Operation(summary = "Calcular pontos",
+            description = "Recurso para calcular o total de pontos que irá ser acumulado em uma compra",
+            responses = @ApiResponse(responseCode = "200", description = "Pontos calculados com sucesso.",
+                    content = @Content(schema = @Schema(implementation = CalculadorResposta.class))))
     public ResponseEntity<CalculadorResposta> calcularPontos(@RequestBody CalculadorDTO calculadorDTO) {
         Double pontosTotais = servico.calcularPontos(calculadorDTO);
         CalculadorResposta resposta = new CalculadorResposta(pontosTotais);
