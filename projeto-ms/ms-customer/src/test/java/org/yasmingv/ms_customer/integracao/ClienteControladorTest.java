6d541f69107694342fcd722deb08f61bec413a35
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-07-03 14:40:08 -0300

    teste buscarCliente controlador

--- projeto-ms/ms-customer/src/test/java/org/yasmingv/ms_customer/integracao/ClienteControladorTest.java
+++ projeto-ms/ms-customer/src/test/java/org/yasmingv/ms_customer/integracao/ClienteControladorTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +11,12 @@ import org.yasmingv.ms_customer.aplicacao.ClienteControlador;
 import org.yasmingv.ms_customer.aplicacao.ClienteServico;
 import org.yasmingv.ms_customer.aplicacao.dto.ClienteDTO;
 
+import java.time.LocalDate;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ClienteControladorTest {
@@ -23,18 +27,63 @@ class ClienteControladorTest {
     @Mock
     private ClienteServico clienteServico;
 
-    @Test
-    void criarCliente_valido_retornarSTTS200() {
+    private ClienteDTO criarClienteDTO() {
         ClienteDTO clienteDTO = new ClienteDTO();
-
-        ClienteDTO svClienteDTO = new ClienteDTO();
-        Mockito.when(clienteServico.salvar(clienteDTO)).thenReturn(svClienteDTO);
-
-        ResponseEntity<ClienteDTO> response = clienteControlador.criarCliente(clienteDTO);
-
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-
-        assertEquals(svClienteDTO, response.getBody());
+        clienteDTO.setCpf("123.456.789-00");
+        clienteDTO.setNome("Marcos Bandrão");
+        clienteDTO.setGenero("Masculino");
+        clienteDTO.setAniversario(LocalDate.of(1990, 1, 1));
+        clienteDTO.setEmail("marcos.band@exemplo.com");
+        clienteDTO.setUrl_foto("examplofoto.jpg");
+        return clienteDTO;
     }
+
+    @Test
+    void criarCliente_valido_retornarSTTS201() {
+        ClienteDTO clienteDTO = criarClienteDTO();
+
+        ClienteDTO clienteSalvoDTO = criarClienteDTO();
+        clienteSalvoDTO.setId(1L);
+
+        when(clienteServico.salvar(any(ClienteDTO.class))).thenReturn(clienteSalvoDTO);
+
+
+        ResponseEntity<ClienteDTO> responseEntity = clienteControlador.criarCliente(clienteDTO);
+
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        ClienteDTO responseBody = responseEntity.getBody();
+        assert responseBody != null;
+        assertEquals(1L, responseBody.getId());
+        assertEquals("Marcos Bandrão", responseBody.getNome());
+        assertEquals("123.456.789-00", responseBody.getCpf());
+        assertEquals("Masculino", responseBody.getGenero());
+        assertEquals(LocalDate.of(1990, 1, 1), responseBody.getAniversario());
+        assertEquals("marcos.band@exemplo.com", responseBody.getEmail());
+        assertEquals("examplofoto.jpg", responseBody.getUrl_foto());
+    }
+
+    @Test
+    void buscarCliente_retornarSTTS200() {
+        Long id = 1L;
+        ClienteDTO clienteDTO = criarClienteDTO();
+        clienteDTO.setId(id);
+
+        when(clienteServico.buscarPorId(id)).thenReturn(clienteDTO);
+
+
+        ResponseEntity<ClienteDTO> responseEntity = clienteControlador.buscarCliente(id);
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        ClienteDTO responseBody = responseEntity.getBody();
+        assert responseBody != null;
+        assertNotNull(responseBody);
+        assertEquals(clienteDTO.getCpf(), responseBody.getCpf());
+        assertEquals(clienteDTO.getNome(), responseBody.getNome());
+        assertEquals(clienteDTO.getGenero(), responseBody.getGenero());
+        assertEquals(clienteDTO.getAniversario(), responseBody.getAniversario());
+        assertEquals(clienteDTO.getEmail(), responseBody.getEmail());
+        assertEquals(clienteDTO.getUrl_foto(), responseBody.getUrl_foto());
+    }
+
 
 }
